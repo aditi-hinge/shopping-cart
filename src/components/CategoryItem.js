09877999fe1254d353f88017237d3a8283c30760
Aditi <95167364+aditi-hinge@users.noreply.github.com>
2022-06-03 13:33:55 +0900

    Category section
    Category section with pictures and information

---
+++ src/components/CategoryItem.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 85vh;
+  position: relative;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+const Info = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Title = styled.h2`
+  color: whitesmoke;
+  margin-bottom: 20px;
+`;
+const Button = styled.button`
+  padding: 1em 1.2em;
+  font-size: 1rem;
+  border: none;
+  background-color: white;
+  color: #34282c;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+export default function CategoryItem({ item }) {
+  return (
+    <Container>
+      <Image src={item.img} />
+      <Info>
+        <Title>{item.title}</Title>
+        <Button>SEE MORE</Button>
+      </Info>
+    </Container>
+  );
+}
---
+++ src/components/data.js
@@ -0,0 +1,48 @@
+import HomePageGirl from "../Images/home-page-girl.jpg";
+import AsianModel from "../Images/asian-model.jpg";
+import BrownModel from "../Images/brown-model.jpg";
+import ShirtModel from "../Images/shirt-model.jpg";
+import YellowLongDress from "../Images/yellow-long-dress.jpg";
+import FormalWear from "../Images/formal-wear.jpg";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: HomePageGirl,
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd"
+  },
+  {
+    id: 2,
+    img: AsianModel,
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed"
+  },
+  {
+    id: 3,
+    img: BrownModel,
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4"
+  }
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: ShirtModel,
+    title: "SHIRT STYLE!"
+  },
+  {
+    id: 2,
+    img: YellowLongDress,
+    title: "LOUNGEWEAR LOVE"
+  },
+  {
+    id: 3,
+    img: FormalWear,
+    title: "LIGHT JACKETS"
+  }
+];
