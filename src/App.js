c427a75ccfd476ca448d431def66278d015e9da0
Aditi <95167364+aditi-hinge@users.noreply.github.com>
2022-06-03 13:35:03 +0900

    Category added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import Navbar from "./components/Navbar";
 import Announcements from "./components/Announcements";
 import Slider from "./components/Slider";
+import Categories from "./components/Categories";
 
 export default function App() {
   return (
@@ -10,8 +11,8 @@ export default function App() {
         <Announcements />
         <Navbar />
         <Slider />
+        <Categories />
       </h1>
     </div>
   );
 }
-
