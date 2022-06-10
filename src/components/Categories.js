8d45c01296211ec72a5ddba709d90e876ede1cac
Aditi <95167364+aditi-hinge@users.noreply.github.com>
2022-06-10 16:26:18 +0900

    responsive categories page

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,18 +1,20 @@
 import styled from "styled-components";
 import CategoryItem from "./CategoryItem";
+import { mobile } from "../responsive";
 import { categories } from "./data";
 
 const Container = styled.div`
   display: flex;
   padding: 20px;
   justify-content: space-between;
+  ${mobile({ padding: "0px", flexDirection: "column" })}
 `;
 
 export default function Categories() {
   return (
     <Container>
       {categories.map((item) => (
-        <CategoryItem item={item} />
+        <CategoryItem item={item} key={item.id} />
       ))}
     </Container>
   );
