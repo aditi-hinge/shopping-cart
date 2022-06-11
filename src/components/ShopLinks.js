ca57cfd11064bf74fcc7abdf8c2a328b43188533
Aditi <95167364+aditi-hinge@users.noreply.github.com>
2022-06-11 17:57:45 +0900

    responsive shopLinks

--- src/components/ShopLinks.js
+++ src/components/ShopLinks.js
@@ -1,27 +1,47 @@
 import styled from "styled-components";
 
+import { mobile } from "../responsive";
+
 const Container = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
   height: 10em;
+
+  ${mobile({
+    height: "2em",
+    justifyContent: "space-around",
+    alignItems: "left"
+  })};
 `;
+
 const Section = styled.div`
   margin: 1em;
   display: block;
+
+  ${mobile({ margin: "0" })};
 `;
 
 const Title = styled.p`
   margin-bottom: 30px;
   font-size: 1.2rem;
   white-space: nowrap;
-  color: #736F6E;
+  color: #736f6e;
+
+  ${mobile({
+    marginBottom: "0",
+    fontSize: "0.7rem",
+    textAlign: "left",
+    textDecoration: "underline"
+  })};
 `;
 
 const List = styled.div`
   margin: 0;
   padding: 0;
   height: 100%;
+
+  ${mobile({ display: "none" })};
 `;
 
 const ListItem = styled.div`
@@ -30,7 +50,7 @@ const ListItem = styled.div`
   font-size: 1.2rem;
   font-weight: 400;
   white-space: nowrap;
-  color: #736F6E;
+  color: #736f6e;
 
   &:hover {
     text-decoration: underline;
